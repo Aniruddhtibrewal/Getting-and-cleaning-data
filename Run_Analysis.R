a7f816f594b2a8dc18109de54689c16d94013fc9
Aniruddhtibrewal <aniruddh1406@gmail.com>
2016-07-04 15:30:52 +1000

    Update Run_Analysis.R

--- Run_Analysis.R
+++ Run_Analysis.R
@@ -1,3 +1,17 @@
+library(reshape2)
+library(data.table)
+
+filename <- "getdata_dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
 setwd("/Users/aniruddhtibrewal/Documents/Training/UCI HAR Dataset")
 
 
